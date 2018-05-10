b1a79a639e1b7381b2f9798ddbfe6a9a005a6ddc
Zach Berta-Thompson <zkbt@mit.edu>
2018-05-10 09:15:12 -0400

    Clean up infrastructure so you can put bounds on the model; add function to fit a FITS file.

--- playground/backgrounds.py
+++ playground/backgrounds.py
@@ -3,10 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.modeling import models, fitting
 from matplotlib.patches import Ellipse
+from craftroom.cmaps import one2another
+okcmap = one2another('red', 'red', alphabottom=0.3, alphatop=0)
 
 def create_test_image(gap=3, N=25):
     '''
-    Create one random crudely-stitched camera, with a Gaussian-ish background to it.
+    Create one random stitched camera, with a Gaussian-ish background to it.
     '''
 
     # generate a random model
@@ -25,21 +27,30 @@ def create_test_image(gap=3, N=25):
     perfect = p_generate(x, y)
     z = np.random.normal(perfect, 1)
 
-    # find the gaps and replace them with zeros
+    # find the gaps, replace them with zeros, and mark them as bad
     bad = (np.abs(x) < gap) | (np.abs(y) < gap)
     z[bad] = 0
+    ok = bad == False
 
-    return x, y, z, p_generate
+    return x, y, z, ok, p_generate
 
-def showimage(x, y, z, **kw):
+def showimage(x, y, z, ok, **kw):
     '''
     Use imshow to show all four CCDs.
     '''
-    plt.imshow( z,
-                origin='lower',
-                extent=[np.min(x), np.max(x), np.min(y), np.max(y)],
-                interpolation='nearest',
-                **kw)
+    imshowkw = dict(origin='lower',
+                    extent=[np.min(x), np.max(x), np.min(y), np.max(y)],
+                    interpolation='nearest', **kw)
+
+    # show the image
+    plt.imshow( z, **imshowkw)
+
+    # mask out the neglected pixels
+    imshowkw['cmap'] = okcmap
+    imshowkw['vmin'] = 0
+    imshowkw['vmax'] = 1
+
+    plt.imshow(ok, **imshowkw)
     plt.axis('scaled')
 
 def bin_image(image, binning=100, visualize=False):
@@ -63,14 +74,64 @@ def bin_image(image, binning=100, visualize=False):
 
     return binned
 
-def fit_2d_gaussian(x, y, z,
-                    imagebinning=1,
-                    ignore=0,
-                    baseline=10,
-                    amplitude=10,
-                    x_mean=0, y_mean=0,
-                    x_stddev=500, y_stddev=500, theta=0,
-                    **kw):
+def guess_2d_gaussian(x, y, z, ok=None):
+    '''
+    Make a guess to initialize the baseline + 2D Gaussian model,
+    based on medians and weighted moments of the image.
+    '''
+
+    if ok == None:
+        ok = np.ones_like(z).astype(np.bool)
+
+    # estimate a baseline flux, from the median of the good pixels
+    baseline_guess = np.median(z[ok])
+
+    # do a *veyr* coarse subtraction, to focus on just the blob
+    crudelysubtracted = np.maximum((z-baseline_guess), 0)
+    amplitude_guess = np.sum(crudelysubtracted)
+
+
+    def moment(q):
+        '''
+        Take the weighted moment of a quantity.
+        '''
+        return np.sum(q[ok]*crudelysubtracted[ok])/np.sum(crudelysubtracted[ok])
+
+    # calculate flux-weighted centroids of the blob
+    x_guess = moment(x)
+    y_guess = moment(y)
+
+    # calculate flux-weighted widths of the blob
+    x_width_guess = np.sqrt(moment((x - x_guess)**2))
+    y_width_guess = np.sqrt(moment((y - y_guess)**2))
+
+
+
+    # create an initial model, with the initial guesses
+    initial = (  models.Const2D(amplitude=baseline_guess) +
+                models.Gaussian2D(amplitude=amplitude_guess,
+                               x_mean=x_guess, y_mean=y_guess,
+                               x_stddev=x_width_guess, y_stddev=y_width_guess,
+                               theta=0.0))
+
+    return initial
+
+
+    # fit it (binning by 100)
+    p = bg.fit_2d_gaussian(x, y, crudelysubtracted, imagebinning=25, baseline=0, amplitude=500,
+                                                           x_mean=x_guess, y_mean=y_guess,
+                                                           x_stddev=500, y_stddev=500, theta=np.pi/4)
+    #plot_fit(xsmall, ysmall, zsmall, truth)
+    bg.plot_fit(x, y, image, p)
+    plt.scatter(x_guess, y_guess, color='green')
+
+    print(f)
+    print(x_guess, y_guess, baseline_guess)
+    #plt.colorbar()
+    #plt.suptitle(f + '\n\n')
+    plt.show()
+
+def fit_model(x, y, z, ok=None, model=None, imagebinning=1, **kw):
     '''
     Fit the scattered light in a list of images (with x, y, z) for each.
 
@@ -78,22 +139,18 @@ def fit_2d_gaussian(x, y, z,
     ----------
     x, y, z : 2D arrays
         The x and y values of each pixel (kind of boring images), and the z flux of the image.
+    ok : 2D array
+        Mask set to 1 where data are OK, 0 where they should be ignored.
+    model : an astropy Model
+        The initialized model to fit.
     imagebinning : int
         By how many pixels should the images be binned?
         This substantially speeds up fitting.
-    ignore : float
-        Ignore pixels that have a particular value.
-        (e.g. Michael set the chip overscans to 0)
-    (others) : float
-        Initial guesses for the parameters of Gaussian2D
+    p_init : an
+        Initial guesses for the parameters of Gaussian2D.
 
     '''
 
-    # create an initial model, with the initial guesses
-    p_init = (models.Const2D(amplitude=baseline) +
-           models.Gaussian2D(amplitude=amplitude,
-                               x_mean=x_mean, y_mean=y_mean,
-                               x_stddev=x_stddev, y_stddev=y_stddev, theta=theta))
 
     # construct the fitter (using LM)
     fit_p = fitting.LevMarLSQFitter()
@@ -102,9 +159,6 @@ def fit_2d_gaussian(x, y, z,
     with warnings.catch_warnings():
         warnings.simplefilter('ignore')
 
-        # pull out only good data
-        ok = z != ignore
-
         # if desired, bin the data
         if imagebinning == 1:
             bx, by, bz, bok = x, y, z, ok
@@ -112,14 +166,15 @@ def fit_2d_gaussian(x, y, z,
             bx = bin_image(x, binning=imagebinning)
             by = bin_image(y, binning=imagebinning)
             bz = bin_image(z, binning=imagebinning)
+            # (exclude bins that are only partially OK)
             bok = bin_image(ok, binning=imagebinning) == 1
 
         # run the fit, starting with initial model and comparing to the data
-        p = fit_p(p_init, bx[bok], by[bok], bz[bok])
+        p = fit_p(model, bx[bok], by[bok], bz[bok])
 
     return p
 
-def plot_2d_gaussian(p, **kw):
+def plot_2d_gaussian(p, color='darkorchid', **kw):
     '''
     Using an astropy model Gaussian2D, plot it on an image.
     **kw go into Ellipse definition
@@ -134,11 +189,11 @@ def plot_2d_gaussian(p, **kw):
                       height=howmanysigma*e['y_stddev'],
                       angle=e['theta']*180/np.pi,
                       linewidth=3, facecolor='none',
-                      edgecolor='darkorchid', alpha=0.5, **kw)
+                      edgecolor=color, alpha=0.5, **kw)
     plt.gca().add_artist(ellipse)
 
 
-def plot_fit(x, y, z, p, cmap='gray', **kw):
+def plot_fit(x, y, z, ok, models=None, colors=None, cmap='gray', **kw):
     '''
     Plot images along with their 2D Gaussian model.
 
@@ -154,27 +209,101 @@ def plot_fit(x, y, z, p, cmap='gray', **kw):
     # plot the data with the best-fit model
     fi, ax = plt.subplots(1, 3, figsize=(8, 2.5), sharex=True, sharey=True)
 
+
+    fitted = models[0]
     plt.sca(ax[0])
-    showimage(x, y, z, vmin=vmin, vmax=vmax, cmap=cmap, **kw)
-    plot_2d_gaussian(p)
-    plt.title("Data")
+    showimage(x, y, z, ok, vmin=vmin, vmax=vmax, cmap=cmap, **kw)
+
+    for m, c in zip(models, colors):
+        plot_2d_gaussian(m, color=c)
+    plt.axis('off')
+    plt.xlabel("Data")
 
     plt.sca(ax[1])
-    showimage(x, y, p(x,y), vmin=vmin, vmax=vmax, cmap=cmap, **kw)
-    plot_2d_gaussian(p)
-    plt.title("Model")
+    showimage(x, y, fitted(x,y), ok, vmin=vmin, vmax=vmax, cmap=cmap, **kw)
+    for m, c in zip(models, colors):
+        plot_2d_gaussian(m, color=c)
+    plt.axis('off')
+    plt.xlabel("Model")
 
     plt.sca(ax[2])
-    showimage(x, y, z-p(x,y), vmin=vmin, vmax=vmax, cmap=cmap, **kw)
-    plot_2d_gaussian(p)
-    plt.title("Residual")
+    showimage(x, y, z-fitted(x,y), ok, vmin=vmin, vmax=vmax, cmap=cmap, **kw)
+    for m, c in zip(models, colors):
+        plot_2d_gaussian(m, color=c)
+    plt.axis('off')
+    plt.xlabel("Residual")
 
-def test():
+def load_camera(filename):
+    '''
+    Load a stitched full camera image.
+
+    Parameters
+    ----------
+    filename : string
+        The path to a FITS file containing faus' background images.
+        (for example, "orbit-4104/bkg_cam1_spm1_1208878859.cal.fits")
+
+    Returns
+    -------
+    x, y, z, ok :
+        The x coordinate, the y coordinate, the image, mask of the good data.
+
+    '''
+
+
+    hdu = fits.open(filename)
+    z = hdu[0].data
+    rows, columns = z.shape
+    x1d = np.arange(columns) - columns/2
+    y1d = np.arange(rows) - rows/2
+    x, y = np.meshgrid(x1d, y1d)
+
+    # faus set the chip gaps to 0
+    ok = z != 0
+
+    return x, y, z, ok
+
+def test(**kw):
+    '''
+    Test out the model fitting on a test image.
+    '''
 
     # create a fake test image
-    x, y, z, truth = create_test_image(N=2048, gap=100)
+    x, y, z, ok, truth = create_test_image(**kw)
+
+    # throw out the corners (flux values of exactly 0 are ignored)
+    r = np.sqrt(x**2 + y**2)
+    incircle = r < np.max(x)
+    ok *= incircle
+    #z[incircle == False] = 0
+
+    # make an initial guess model
+    initial = guess_2d_gaussian(x,y,z)
 
     # fit it (binning by 100)
-    p = fit_2d_gaussian(x, y, z, imagebinning=100)
+    fitted = fit_model(x, y, z, model=initial)
+
     #plot_fit(xsmall, ysmall, zsmall, truth)
-    plot_fit(x, y, z, p)
+    plot_fit(x, y, z, ok, models=[fitted, initial, truth], colors=['darkorchid', 'gray', 'hotpink'])
+
+
+def fit_camera(filename):
+    '''
+    Fit an image from a full camera.
+    '''
+
+    # load an image from a FITS file
+    x, y, z, ok = load_camera(filename)
+
+    # throw out the corners
+    r = np.sqrt(x**2 + y**2)
+    ok *= r < np.max(x)*1.2
+
+    # make an initial guess model
+    initial = guess_2d_gaussian(x,y,z)
+
+    # fit it (binning by 100)
+    fitted = fit_model(x, y, z, model=initial)
+
+    #plot_fit(xsmall, ysmall, zsmall, truth)
+    plot_fit(x, y, z, ok, models=[fitted, initial], colors=['darkorchid', 'gray'])
