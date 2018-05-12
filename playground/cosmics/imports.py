3ec3e190f2a3f682d17d1e7b40f75c9f7286aae8
Zach Berta-Thompson <zkbt@mit.edu>
2018-05-12 11:59:28 -0400

    trying to fix display problem

--- playground/cosmics/imports.py
+++ playground/cosmics/imports.py
@@ -1,18 +1,13 @@
 # this sets where to look for inputs and store outputs
 # (it will default to `~/.tess/spyffi` if the $SPYFFIDATA environment variable isn't set)
-import os
+#import os
 #os.environ["SPYFFIDATA"] = '/Users/zkbt/Cosmos/Data/TESS/FFIs'
 
 import matplotlib as mpl
-mpl.rcParams['axes.labelsize'] = 8#'small'
-mpl.rcParams['xtick.labelsize'] = 8#'small'
-mpl.rcParams['ytick.labelsize'] = 8#'small'
-mpl.rcParams['font.size'] = 8#'small'
 
 # some basics
 import numpy as np, matplotlib.pyplot as plt
 import os, copy, subprocess, glob
-
 import matplotlib.animation as ani
 
 from .talker import Talker
