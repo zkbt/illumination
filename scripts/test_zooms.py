c0c45577b519638997e04bf59627a1237cb2f4bd
Zach Berta-Thompson <zkbt@mit.edu>
2018-05-15 17:18:42 -0400

    starting to get multicamera visualization working

--- scripts/test_zooms.py
+++ scripts/test_zooms.py
@@ -1,18 +1,23 @@
 from playground.cosmics.stamps import *
 from playground.tv.frames import *
-from playground.tv.illustrations import FourCameraIllustration
+from playground.tv.illustrations import FourCameraIllustration, ZoomsIllustration
 from playground.tv.animation import animate
 
 N = 4184
 stamps = [create_test_stamp(cadence=2,
                             col_cent=np.random.randint(0, N),
                             row_cent=np.random.randint(0, N),
-                            cam=np.random.randint(0, 4))
-          for i in range(3)]
+                            cam=np.random.randint(0, 4)+1)
+          for i in range(10)]
+for i, s in enumerate(stamps):
+    s.static['TIC_ID'] += 1
 
 fci = FourCameraIllustration()
-
-
+zi = ZoomsIllustration(stamps)
+for i in range(4):
+    del zi.frames[i+1]
+zi.plot()
+animate(zi)
 #fos.plot()
 #animate(ic)
 '''
