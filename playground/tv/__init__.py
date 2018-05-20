6211b57dd3fea9feface4fd6a73d3dc3a61c2568
Zach Berta-Thompson <zach.bertathompson@colorado.edu>
2018-05-20 09:19:27 -0600

    easier imports

--- playground/tv/__init__.py
+++ playground/tv/__init__.py
@@ -4,3 +4,8 @@ TESS pixels in Python and matplotlib. Hopefully
 it'll be useful for automated visualizations
 and for animations.
 '''
+
+from .tools import illustratefits
+from .animation import animate
+from .zoom import *
+from .illustrations import *
