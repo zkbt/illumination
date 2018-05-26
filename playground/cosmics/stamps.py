186f721cbab107eaa6f57c7e94951786ca1abeb5
Zach Berta-Thompson <zkbt@mit.edu>
2018-05-26 15:17:59 -0600

    add kludge for ccd to camera

--- playground/cosmics/stamps.py
+++ playground/cosmics/stamps.py
@@ -124,11 +124,11 @@ class Stamp(Cube):
 				photons[i,:,:] = d.T
 			else:
 				photons[i,:,:] = d
-		if flip:
-			self.speak('applied a KLUDGE to CCD1 + CCD2 (COL_CENT<2136?)')
 
 		self.__init__(self, photons=photons, temporal=temporal, spatial=spatial, static=static)
 		self.speak('populated {}'.format(self))
+		if flip:
+			self.speak('applied a KLUDGE to CCD1 + CCD2 (COL_CENT<2136?)')
 
 	def filename(self, directory='.'):
 		'''The base filename for this stamp.'''
