c9b26f57eae8a36dab9639d178497138c4e91e63
Zach Berta-Thompson <zkbt@mit.edu>
2018-06-10 04:49:40 -0600

    add stamps script for 8193

---
+++ scripts/stamps_8193.py
@@ -0,0 +1,11 @@
+from playground.imports import *
+from playground.cosmics.stamps import *
+
+orbit = 'orbit-8193'
+sparse_pattern = '/pdo/ramp/orbit-8193/*sparse.fits'
+
+orbit_directory = os.path.join('/pdo/ramp/zkbt', orbit)
+mkdir(orbit_directory)
+stamps_directory = os.path.join(orbit_directory, 'stamps')
+
+process_sparse_into_stamps(sparse_pattern=sparse_pattern, stamps_directory=stamps_directory, ntimes=10, nstars=5)
