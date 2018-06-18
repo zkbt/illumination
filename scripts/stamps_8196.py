dc91065991fb859c06e2234cb8e932dcf8e6484f
Zach Berta-Thompson <zkbt@mit.edu>
2018-06-17 18:04:35 -0600

    reorganize sparse splitting

--- scripts/stamps_8196.py
+++ scripts/stamps_8196.py
@@ -1,5 +1,5 @@
-from playground.imports import *
-from playground.cosmics.stamps import *
+from playground.process_sparse import *
+
 
 orbit = 'orbit-8196'
 sparse_pattern = '/pdo/ramp/orbit-8196/subarrays/cam*/*subarrays.fits'
@@ -8,5 +8,5 @@ orbit_directory = os.path.join('/pdo/ramp/zkbt', orbit)
 mkdir(orbit_directory)
 stamps_directory = os.path.join(orbit_directory, 'stamps')
 
-split_times_to_stars(sparse_pattern, stamps_directory='stamps', ntimes=None, nstars=None):
+split_times_to_stars(sparse_pattern, stamps_directory='stamps', ntimes=None, nstars=None)
 #process_sparse_into_stamps(sparse_pattern=sparse_pattern, stamps_directory=stamps_directory, ntimes=5, nstars=None)
