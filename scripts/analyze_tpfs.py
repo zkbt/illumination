e314708dcb274f9e7668a44375dd1b00dde76ed9
Zach Berta-Thompson <zkbt@mit.edu>
2018-07-01 22:31:41 -0600

    kludge to make run OK over night

--- scripts/analyze_tpfs.py
+++ scripts/analyze_tpfs.py
@@ -15,13 +15,20 @@ span = np.median(ends - starts)
 print('splitting {} days {} ways leaves {:.0f}/{:.0f} (30min/2min) cadences per group'.format(end-start, N, span*24/0.5, span*24*60/2))
 
 stampfiles = glob.glob('/scratch2/zkbt/orbit-8196/stamps/*/*.npy')
-stampfiles
+outputdirectory = '/scratch2/zkbt/orbit-8196/analyses'
 
+strategy = Central(10)
 starts = [-np.inf] + list(starts)
 ends = [np.inf] + list(ends)
 for start, end in zip(starts, ends):
     for s in stampfiles:
         for cadence in [120, 1800]:
-            tpf = EarlyTessTargetPixelFile.from_stamp(Stamp(s))
-            tpfs, lcs, summary = evaluate_strategy(tpf, directory='/scratch2/zkbt/orbit-8196/analyses', cadence=cadence, strategy=Central(10), start=start, end=end)
+            #tic = os.path.basename(s).split('tic')[1].split('_')[0]
+            #search = os.path.join(outputdirectory, strategy.name.replace(' ', ''), '*tic{}*{}s'.format(tic, cadence))
+            try:
+                tpf = EarlyTessTargetPixelFile.from_stamp(Stamp(s))
+                tpf.to_fits(directory=outputdirectory)
+                tpfs, lcs, summary = evaluate_strategy(tpf, directory=outputdirectory, cadence=cadence, strategy=strategy, start=start, end=end)
+            except:
+                print("Something went wrong with {}!".format(s))
             visualize_strategy(tpfs, lcs, summary);
--- scripts/make_tpfs_from_subarrays.py
+++ scripts/make_tpfs_from_subarrays.py
@@ -7,8 +7,8 @@ files = np.sort(glob.glob('/scratch2/zkbt/orbit-8196/subarrays/cam4/cam*.fits'))
 # this is the bright star Chelsea and Andrew were looking at
 ext = 418
 
-t = EarlyTessTargetPixelFile.from_sparse_subarrays(files[0:1000], extension=ext)
-t.to_fits('/scratch2/zkbt/orbit-8196/{}-short-targ.fits'.format(t.tic_id))
+t = EarlyTessTargetPixelFile.from_sparse_subarrays(files[0:100], extension=ext)
+t.to_fits('/scratch2/zkbt/orbit-8196/{}-short-targ.fits'.format(t.tic_id), overwrite=True)
 
 t = EarlyTessTargetPixelFile.from_sparse_subarrays(files, extension=ext)
-t.to_fits('/scratch2/zkbt/orbit-8196/{}}-targ.fits'.format(t.tic_id))
+t.to_fits('/scratch2/zkbt/orbit-8196/{}}-targ.fits'.format(t.tic_id), overwrite=True)
