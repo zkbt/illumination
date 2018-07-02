fdf72fddbbb35f1bfb362a80565d781917b86bad
Zach Berta-Thompson <zkbt@mit.edu>
2018-07-01 19:29:24 -0600

    fix headers on loading TPFs from sparse subarrays

--- scripts/make_tpfs_from_subarrays.py
+++ scripts/make_tpfs_from_subarrays.py
@@ -7,7 +7,7 @@ files = glob.glob('/scratch2/zkbt/orbit-8196/subarrays/cam4/cam*.fits')
 # this is the bright star Chelsea and Andrew were looking at
 ext = 418
 
-t = EarlyTessTargetPixelFile.from_sparse_subarrays(files[0:10000], ext=ext)
+t = EarlyTessTargetPixelFile.from_sparse_subarrays(files[0:1000], ext=ext)
 t.to_fits('/scratch2/zkbt/orbit-8196/{}-short-targ.fits'.format(t.tic_id))
 
 t = EarlyTessTargetPixelFile.from_sparse_subarrays(files, ext=ext)
