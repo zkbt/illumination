7329537f4afaf7e5ece34b04428e9bf3f87f7f65
Zach Berta-Thompson <zkbt@mit.edu>
2018-07-15 21:03:15 -0600

    tiny fixes on ETF

--- playground/tv/frames/EmptyTimeseriesFrame.py
+++ playground/tv/frames/EmptyTimeseriesFrame.py
@@ -3,17 +3,37 @@ from ..sequence import make_sequence
 
 class EmptyTimeseriesFrame(FrameBase):
 	'''
-	An EmptyTimeseriesFrame has a vertical bar
-	indicating the time, which can move when animated.
+	An EmptyTimeseriesFrame has a vertical bar for time when animated.
 
-	Other data can be plotted into the axes,
-	after the illustration has been plotted once.
-
+	The frame should be added to an illustration,
+	the illustration should be plotted,
+	and then data can be plotted into the
+	ETF.ax and ETF.ax_hist frames.
 	'''
 
 	frametype = 'timeseries'
-	def __init__(self, xlim=[None, None], ylim=[None, None], title='', histogram=True, name='timeseries', **kwargs):
+	def __init__(self, name='timeseries', xlim=[None, None], ylim=[None, None], ylabel='', histogram=True,**kwargs):
+		'''
+		Initialize an empty timeseries frame.
 
+		Parameters
+		----------
+
+		name : string
+			A custom name for this frame.
+
+		xlim : tuple
+			Set the xlimits. If None, wait for data to appear.
+
+		ylim : tuple
+			Set the ylimits. If None, wait for data to appear.
+
+		ylabel : string
+			Set a ylabel for this plot.
+
+		histogram : bool
+			Should we add an extra axis for a histogram?
+		'''
 		# initialize an empty frame
 		FrameBase.__init__(self, name=name, **kwargs)
 
@@ -22,30 +42,27 @@ class EmptyTimeseriesFrame(FrameBase):
 		self.ymin, self.ymax = ylim
 
 		# store the custom title for this one
-		self.titlefordisplay =  title
+		self.titlefordisplay =  ylabel
 
 		# should this frame have a histrogram attached to it?
 		self.histogram = histogram
 
 	def plot(self,  **kwargs):
 		'''
-		Add a vertical line, and set the limits.
+		Make an empty plot, with a vertical line waiting in it.
 		'''
 
 		self.plotted = {}
-
-		# make sure we point back at this frame
-		plt.sca(self.ax)
 
 		if self.histogram:
 			b = self.ax.get_position()
 			#self.ax_hist = self.ax.figure.add_axes([b.x0, b.y0, b.x1, b.y1])
 
 		# plot the time bar
-
+		self.plotted['vline'] = self.ax.axvline(np.nan, color='gray', alpha=0.5, zorder=-1e6)
 
 		# pull the title for this frame
-		plt.ylabel(self.titlefordisplay)
+		self.ax.set_ylabel(self.titlefordisplay)
 		if self.ax.get_xticklabels()[0].get_visible():
 			plt.xlabel('Time - {:.5f} (days)'.format(self.offset))
 
@@ -61,24 +78,6 @@ class EmptyTimeseriesFrame(FrameBase):
 		if self.ymin is not None and self.ymax is not None:
 			plt.ylim(self.ymin, self.ymax)
 
-	"""
-	@property
-	def offset(self):
-		try:
-			return self._offset
-		except AttributeError:
-			try:
-				self._offset = np.min(self.illustration._gettimes().jd)
-			except AttributeError:
-				self._offset = np.min(self._gettimes().jd)#Time(0, format='jd')
-			return self._offset
-
-	def _timestring(self, time):
-		'''
-		Return a string, given an input time (still in spacecraft time).
-		'''
-		return 't={:.5f}{:+.5f}'.format(self.offset, time.jd-self.offset)
-	"""
 	def update(self, time):
 		'''
 		Update this frame to a particular time (for use in animations).
@@ -86,10 +85,7 @@ class EmptyTimeseriesFrame(FrameBase):
 
 		# add a vertical line
 		v = time.jd - self.offset
-		try:
-			self.plotted['vline'].set_xdata(v)
-		except KeyError:
-			self.plotted['vline'] = self.ax.axvline(v, color='gray', alpha=0.5, zorder=-1e6)
+		self.plotted['vline'].set_xdata(v)
 
 		# add a time label
 		#s = self._timestring(time)
