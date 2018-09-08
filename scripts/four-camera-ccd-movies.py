68c2c95590c97394046ff9adaf4f1e0c5e29d7bf
Zach Berta-Thompson <zkbt@mit.edu>
2018-09-08 17:57:54 -0600

    big movie script

--- scripts/four-camera-ccd-movies.py
+++ scripts/four-camera-ccd-movies.py
@@ -1,43 +1,45 @@
 from playground.tv import *
-for orbit in [9,10]:
-    N = 3
-    directory = '/pdo/ramp/orbit-{}/cal_ffi'.format(orbit)
+for N in [25, None]:
+    for processingsteps in [['subtractmean'], []]:
+        for orbit in [9,10]:
 
-    # compile a dictionary of cameras, with a subdictionary of ccds
-    cameras = {}
-    for camera in [1,2,3,4]:
+            directory = '/pdo/ramp/orbit-{}/cal_ffi'.format(orbit)
 
-        # include each CCD individually
-        ccds = {}
-        for ccd in [1,2,3,4]:
+            # compile a dictionary of cameras, with a subdictionary of ccds
+            cameras = {}
+            for camera in [1,2,3,4]:
 
-
-            # pull out the files for this single CCD
-            filepattern = 'tess*-*-{camera}-crm-ffi_ccd{ccd}.cal.fits'
-            pattern = os.path.join(directory, filepattern.format(**locals()))
-            files = list(np.sort(glob.glob(pattern)))[:N]
-
-            # make a sequence out of these images
-            sequence = make_sequence(files, ext_image=0, use_headers=False, use_filenames=True, timekey='cadence')
-
-            # load a median image, instead of computing it
-            #medianfilename = '/pdo/ramp/qlp_data/orbit-9/ffi/cam{camera}/ccd{ccd}/sub/Median.fits'.format(**locals())
-            #sequence.spatial['median']  = fits.open(medianfilename)[0].data
-
-            ccds['ccd{}'.format(ccd)] = sequence
-        cameras['cam{}'.format(camera)] = ccds
+                # include each CCD individually
+                ccds = {}
+                for ccd in [1,2,3,4]:
 
 
-    i = FourCameraOfCCDsIllustration(processingsteps=['subtractmedian'],**cameras)
-    i.plot()
-    filename = 'subtracted-orbit{}-four-camera.pdf'.format(orbit)
-    i.savefig(filename, dpi=300)
-    i.animate(filename.replace('pdf', 'mp4'), cadence=1.0*u.s)
+                    # pull out the files for this single CCD
+                    filepattern = 'tess*-*-{camera}-crm-ffi_ccd{ccd}.cal.fits'
+                    pattern = os.path.join(directory, filepattern.format(**locals()))
+                    files = list(np.sort(glob.glob(pattern)))[:N]
 
-    # make both a raw, and a differenced movie
-    for cam in cameras.keys():
-        i = CameraOfCCDsIllustration(processingsteps=['subtractmedian'],**cameras[cam])
-        i.plot()
-        filename = 'subtracted-orbit{}-{}.pdf'.format(orbit, cam)
-        i.savefig(filename, dpi=300)
-        i.animate(filename.replace('pdf', 'mp4'), cadence=1.0*u.s)
+                    # make a sequence out of these images
+                    sequence = make_sequence(files, ext_image=0, use_headers=False, use_filenames=True, timekey='cadence')
+
+                    # load a median image, instead of computing it
+                    #medianfilename = '/pdo/ramp/qlp_data/orbit-9/ffi/cam{camera}/ccd{ccd}/sub/Median.fits'.format(**locals())
+                    #sequence.spatial['median']  = fits.open(medianfilename)[0].data
+
+                    ccds['ccd{}'.format(ccd)] = sequence
+                cameras['cam{}'.format(camera)] = ccds
+
+
+            i = FourCameraOfCCDsIllustration(processingsteps=processingsteps,**cameras)
+            i.plot()
+            filename = 'orbit{}-four-camera-{}-limit{}.pdf'.format(orbit, '-'.join(processingsteps), N)
+            i.savefig(filename, dpi=300)
+            i.animate(filename.replace('pdf', 'mp4'), cadence=1.0*u.s)
+
+            # make both a raw, and a differenced movie
+            for cam in cameras.keys():
+                i = CameraOfCCDsIllustration(processingsteps=processingsteps,**cameras[cam])
+                i.plot()
+                filename = 'orbit{}-{}-{}-limit{}.pdf'.format(orbit, cam, '-'.join(processingsteps), N))
+                i.savefig(filename, dpi=300)
+                i.animate(filename.replace('pdf', 'mp4'), cadence=1.0*u.s)
